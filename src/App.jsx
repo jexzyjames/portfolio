0315da1023df4d60d670dd1885bf215419975ce2
jexzyjames <jexzyjames@gmail.com>
2025-03-12 11:34:55 +0100

    railway hosting issues fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React,{useState,useEffect} from 'react'
+import React,{useState} from 'react'
 import Main from './Portfolio/Components/Main/Main';
 import About from './Portfolio/Components/About/About';
 import Projects from './Portfolio/Components/Projects/Projects';
