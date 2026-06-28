d12d5da9f472563f92a40791c3ca24d302d9585c
James Jegede <jexzyjames@gmail.com>
2026-06-28 15:19:52 +0100

    Refactor Contact component for email sending

--- src/Portfolio/Contact/Contact.jsx
+++ src/Portfolio/Contact/Contact.jsx
@@ -176,7 +176,7 @@ useEffect(() => {
               <Button
                 className="submit-btn"
                 type="submit"
-                onClick = { (e)=> sendEmail(e) }
+                onClick = { ()=> sendEmail() }
                 sx={{
                   margin: "20px 0",
                   borderRadius: "50px",
